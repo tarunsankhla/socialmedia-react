78b4db7536ac40e1e6ace3aabe18dd601a299ed9
Tether <tarun.sankhla.prc@gmail.com>
2022-05-07 07:28:44 +0530

    basic

---
+++ src/Main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+    <div className='main-container'>    
+        {/* <Navbar />
+        <div className='main-body'>
+            <AsideNavbar />
+            <span  className='outlet-body'>
+                <Outlet />
+            </span>
+                {loginShow ?
+                    <LoginPage props={setLoginShow} />
+                    : <SignUpPage  props={setLoginShow} />
+             }
+            <ToastContainer style={{ fontSize: "1.5em" }} />
+        </div>
+        <Footer /> */}
+    </div>
+</>
+  )
+}
+
+export default Main
