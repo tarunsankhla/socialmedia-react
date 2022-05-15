980af67dd9fc46d963867eede4a09832125c3e32
Tether <tarun.sankhla.prc@gmail.com>
2022-05-16 00:02:07 +0530

    prfile page

--- src/components/UI/Buttons/buttons.js
+++ src/components/UI/Buttons/buttons.js
@@ -1,7 +1,7 @@
 import "./buttons.css";
 
 function NormalButton(props) { 
-    return <button className="normal-btn" style={{backgroundColor: `${props.color}` && "red"}} >{ props.name}</button>
+    return <button className="normal-btn"  style={{backgroundColor: `${props.color}` || "red", padding : `${props.padding}` || "3px"}} >{ props.name}</button>
 }
 
 export { 
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,3 +1,6 @@
+import { NormalButton } from 'components/UI/Buttons/buttons';
+import { firestore } from 'firebase.config';
+import { doc, getDoc } from 'firebase/firestore';
 import React , { useEffect,useState }from 'react';
 import { useParams } from 'react-router';
 import { GetIndividualUserData } from 'utils/authService';
@@ -8,11 +11,45 @@ const ProfilePage = () => {
   const [userData, setUserData] = useState({});
   console.log(userID);
   useEffect(() => {
-    GetIndividualUserData(userID, setUserData);
+    const userRef = doc(firestore, `users/${userID}`);
+    (async () => {
+      try {
+        const response = await getDoc(userRef);
+        console.log(response.data(), response.id, setUserData, userID);
+        console.log(response.data()[userID]);
+        setUserData(response.data()[userID]);
+        // setData(res1.data() ?? {});
+      } catch (err) {
+        console.log(err.message)
+        // }
+        alert("error", err.message);
+      }
+    })();
    },[])
   return (
-    <div>
-      { userData?.payload?.name}
+    <div className='flex main-profile-page'>
+      <div className='profile-page-header-container flex'>
+        {userData?.photo?.length ?
+          <img src={userData.photo} className='handle-img-np user-img profile-img' />
+          : <span className='handle-img-np profile-digit'>
+              {userData?.name ? userData?.name[0].toUpperCase() : "D"}
+            </span>}
+        <p className='fn-wg-700 profile-page-name'>{userData?.name}</p>
+        <p className='gray-txt'>{"@" + userData?.emailId?.split("@")[0]}</p>
+        <NormalButton name="Edit Profile" color="red" padding="7px 1em" />
+        <p>{userData?.bio || 'No Bio'}</p>
+      </div>
+      <div className='flex profile-page-stats-container'>
+        <div>
+          <p className='fn-wg-800'>{ userData?.following?.length}</p>
+          <p>Following</p></div>
+        <div>
+          <p className='fn-wg-800'>D</p>
+          <p>Posts</p></div>
+        <div>
+          <p className='fn-wg-800'>{ userData?.followers?.length}</p>
+          <p>Followers</p></div>
+      </div>
     </div>
   )
 }
