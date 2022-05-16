f632ce1769171e052041b5ee8ab3435962efbe65
Tether <tarun.sankhla.prc@gmail.com>
2022-05-17 03:29:26 +0530

    navbar

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import { useUserData } from 'context/UserContext';
 import React from 'react';
+import { useNavigate as Navigate } from 'react-router';
 import "./Navbar.css";
 
 const Navbar = () => {
   const { userData } = useUserData();
+  const navigate = Navigate();
   return (
     <div className='Navbar'>
       {/* <img src={LogoWEBP1} loading="lazy" alt='logo' className='logo-navbar' /> */}
-      SpaceVerse
+      <p className='xlg-txt' onClick={() => navigate("/")}
+          style={{cursor:"pointer"}}>
+        SpaceVerse</p>
       <p>{ userData?.emailId ? "@" + userData?.emailId?.split("@")[0] : ""}</p>
   
     </div>
