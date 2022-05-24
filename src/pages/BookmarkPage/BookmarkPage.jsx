c4b976d024bdb66947cd32feccf0024baae8a3f6
Tether <tarun.sankhla.prc@gmail.com>
2022-05-24 18:50:23 +0530

    bookb=mark handle

--- src/pages/BookmarkPage/BookmarkPage.jsx
+++ src/pages/BookmarkPage/BookmarkPage.jsx
@@ -73,11 +73,13 @@ const BookmarkPage = () => {
       <div className='pd-10 flex-cln'>
         <h1 className='xxlg-txt fn-wg-700 pd-10'>Your Bookmarks</h1>
         <div> {
-                bookmarkPostArray.map(i => (
+            bookmarkPostArray?.length !== 0 ?
+                bookmarkPostArray?.map(i => (
                     <Post props={i}
                         key={i.postid + i.createdTime} />
                    
-                ))
+                )) 
+                    : <p className='xxlg-txt gray-txt fn-wg-700'>No BookMark!!</p>
             } </div>
         </div>
     )
